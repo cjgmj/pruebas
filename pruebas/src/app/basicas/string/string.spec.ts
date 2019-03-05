73c120157cd8a3e744e0f2149f3a8f250b327ac1
cjgmj <cjgmj@hotmail.com>
2019-03-05 17:04:11 +0100

    Comprobar que el resultado contenga una cadena

--- pruebas/src/app/basicas/string/string.spec.ts
+++ pruebas/src/app/basicas/string/string.spec.ts
@@ -8,4 +8,11 @@ describe('Pruebas de strings', () => {
 
         expect( typeof resp ).toBe('string');
     });
+
+    it('Debe devolver un saludo con el nombre enviado', () => {
+        const nombre = 'Juan';
+        const resp = mensaje(nombre);
+
+        expect( resp ).toContain(nombre);
+    });
 });
