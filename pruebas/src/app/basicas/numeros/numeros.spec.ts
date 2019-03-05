556fa4157ead6bc58f005d245335e94b914c0224
cjgmj <cjgmj@hotmail.com>
2019-03-05 17:09:43 +0100

    Pruebas números

---
+++ pruebas/src/app/basicas/numeros/numeros.spec.ts
@@ -0,0 +1,15 @@
+import { incrementar } from './numeros';
+
+describe('Pruebas de números', () => {
+    it('Debe devolver 100, si el número introducido es mayor a 100', () => {
+        const resp = incrementar(300);
+
+        expect( resp ).toBe(100);
+    });
+
+    it('Debe devolver el número ingresado más uno, si no es mayor a 100', () => {
+        const resp = incrementar(50);
+
+        expect( resp ).toBe(51);
+    });
+});
