1e2ec42ad2d44ea3d5fd304fe307b765223d8a7b
cjgmj <cjgmj@hotmail.com>
2019-03-18 17:38:09 +0100

    Configuraciones pruebas de integración

---
+++ pruebas/src/app/intermedio2/medico/medico.component.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { MedicoComponent } from './medico.component';
+
+describe('Medico Component', () => {
+    let componente: MedicoComponent;
+    let fixture: ComponentFixture<MedicoComponent>;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [MedicoComponent]
+        });
+
+        fixture = TestBed.createComponent(MedicoComponent);
+        componente = fixture.componentInstance;
+    });
+
+    it('', () => {
+
+    });
+});
