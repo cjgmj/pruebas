e050402a62fb08494c306658ac732f01d263c946
cjgmj <cjgmj@hotmail.com>
2019-03-19 17:00:15 +0100

    Pruebas de router-outlet

--- pruebas/src/app/app.component.spec.ts
+++ pruebas/src/app/app.component.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -7,6 +10,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes([])
+      ]
     }).compileComponents();
   }));
 
@@ -22,10 +28,18 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('pruebas');
   });
 
-  it('should render title in a h1 tag', () => {
+  xit('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to pruebas!');
+  });
+
+  it('Debe tener un router-outlet', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const debugElement = fixture.debugElement.query( By.directive(RouterOutlet) );
+
+    expect( debugElement ).not.toBeNull();
   });
 });
