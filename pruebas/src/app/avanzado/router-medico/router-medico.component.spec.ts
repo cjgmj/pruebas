181aa33ddfdc8873aab107f57b50f4c2b322523c
cjgmj <cjgmj@hotmail.com>
2019-03-19 17:29:16 +0100

    Reemplazar servicios

---
+++ pruebas/src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -0,0 +1,44 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterMedicoComponent } from './router-medico.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, empty } from 'rxjs';
+
+class FakeRouter {
+  navigate( params ) { }
+}
+
+class FakeActivatedRoute {
+  params: Observable<any> = empty();
+}
+
+describe('RouterMedicoComponent', () => {
+  let component: RouterMedicoComponent;
+  let fixture: ComponentFixture<RouterMedicoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RouterMedicoComponent ],
+      providers: [
+        { provide: Router, useClass: FakeRouter },
+        { provide: ActivatedRoute, useClass: FakeActivatedRoute }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RouterMedicoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('Debe redireccionar a Médico cuando se guarde', () => {
+    const router = TestBed.get(Router);
+    const spy = spyOn(router, 'navigate');
+
+    component.guardarMedico();
+
+    expect(spy).toHaveBeenCalledWith(['medico', '123']);
+  });
+});
